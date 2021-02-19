7ba219f8e259604bd49e5a7c9a97650fa8dfd176
FilipDavid1 <filipenkodavid@gmail.com>
2021-02-19 20:15:29 +0100

    Add login page with button event handler

--- TurnamentManager/TurnamentManager/MainPage.xaml.cs
+++ TurnamentManager/TurnamentManager/MainPage.xaml.cs
@@ -14,5 +14,20 @@ namespace TurnamentManager
         {
             InitializeComponent();
         }
+        private void LogInButton_OnClicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(EmailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+            if (isEmailEmpty != true && isPasswordEmpty != true)
+            {
+                DisplayAlert("TRUE ALERT", "it worked", "OK");
+            }
+
+            else
+            {
+                DisplayAlert("FALSE ALERT", "it worked", "OK");
+            }
+        }
     }
 }
