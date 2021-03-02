724e65fc42e7ae4628eac64cc581a08dda087ce1
FilipDavid1 <filipenkodavid@gmail.com>
2021-03-02 21:48:15 +0100

    Add button for picking images from gallery

--- TurnamentManager/TurnamentManager/CreateTournamentPage.xaml.cs
+++ TurnamentManager/TurnamentManager/CreateTournamentPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,26 @@ namespace TurnamentManager
         public CreateTournamentPage()
         {
             InitializeComponent();
+        }
+        async void MenuItem_OnClicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not supported", "not supported", "ok");
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+            if (selectedImage == null)
+            {
+                await DisplayAlert("Error", "could not ger image", "ok");
+            }
+            selectedImage.Source = ImageSource.FromStream(()=> selectedImageFile.GetStream());
         }
     }
 }
