219c979dc490805966d6659b6cb9f9751be6eaca
FilipDavid1 <filipenkodavid@gmail.com>
2021-03-03 10:48:25 +0100

    Change sport and format buttons to pickers

--- TurnamentManager/TurnamentManager/TeamPage.xaml.cs
+++ TurnamentManager/TurnamentManager/TeamPage.xaml.cs
@@ -18,6 +18,25 @@ namespace TurnamentManager
             InitializeComponent();
         }
 
-        
+        async void MenuItem_OnClicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not supported", "not supported", "ok");
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+            if (selectedImage == null)
+            {
+                await DisplayAlert("Error", "could not ger image", "ok");
+            }
+            selectedImage.Source = ImageSource.FromStream(()=> selectedImageFile.GetStream());
+        }
     }
 }
