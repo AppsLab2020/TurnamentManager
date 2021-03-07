ca6b67f1294a6de249b474b480322cc6fd29db0d
FilipDavid1 <filipenkodavid@gmail.com>
2021-03-07 20:39:26 +0100

    Add entry for player name and picker for player qualities

--- TurnamentManager/TurnamentManager/Views/TeamPage.xaml.cs
+++ TurnamentManager/TurnamentManager/Views/TeamPage.xaml.cs
@@ -14,28 +14,6 @@ namespace TurnamentManager.Views
             InitializeComponent();
         }
 
-        private async void MenuItem_OnClicked(object sender, EventArgs e)
-        {
-            await CrossMedia.Current.Initialize();
-
-            if (!CrossMedia.Current.IsPickPhotoSupported)
-            {
-                await DisplayAlert("Not supported", "not supported", "ok");
-            }
-
-            var mediaOptions = new PickMediaOptions()
-            {
-                PhotoSize = PhotoSize.Medium
-            };
-            
-            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
-            
-            if (selectedImage == null)
-            {
-                await DisplayAlert("Error", "could not ger image", "ok");
-            }
-            
-            selectedImage.Source = ImageSource.FromStream(()=> selectedImageFile.GetStream());
-        }
+       
     }
 }
