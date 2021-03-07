17543c73dca20f07feddf8c9d83da403fb0657f3
FilipDavid1 <filipenkodavid@gmail.com>
2021-03-07 20:34:01 +0100

    Add + button event handler

---
+++ TurnamentManager/TurnamentManager/Views/PlayerPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TurnamentManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayerPage : ContentPage
+    {
+        public PlayerPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CreatePlayerPage());
+        }
+    }
+}
