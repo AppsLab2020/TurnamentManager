a35aa521f50c2cbb85dc75a3f8cafe891e6b70d9
FilipDavid1 <filipenkodavid@gmail.com>
2021-03-07 20:35:03 +0100

    Add CreatePlayerPage

---
+++ TurnamentManager/TurnamentManager/Views/CreatePlayerPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TurnamentManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreatePlayerPage : ContentPage
+    {
+        public CreatePlayerPage()
+        {
+            InitializeComponent();
+        }
+        
+        private async void MenuItem_OnClicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not supported", "not supported", "ok");
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+            
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+            
+            if (selectedImage == null)
+            {
+                await DisplayAlert("Error", "could not ger image", "ok");
+            }
+            
+            selectedImage.Source = ImageSource.FromStream(()=> selectedImageFile.GetStream());
+        }
+    }
+}
