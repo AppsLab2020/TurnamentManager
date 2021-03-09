5658055dc9f3733e7f567f24d4cfe912152b3a47
jakkappro <jakkappro@gmail.com>
2021-03-09 14:47:01 +0100

    Add player and saving player

---
+++ TurnamentManager/TurnamentManager/Classes/Tournament/Team.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TurnamentManager.Classes.Tournament
+{
+    public class Team
+    {
+        public readonly List<Player> Players;
+
+        public Team()
+        {
+            Players = new List<Player>();
+        }
+    }
+}
