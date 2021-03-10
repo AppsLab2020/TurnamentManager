b0642674a071ee8565b2561bf42a413f4e4d386b
FilipDavid1 <filipenkodavid@gmail.com>
2021-03-10 19:44:34 +0100

    Add icon for player page

--- TurnamentManager/TurnamentManager.Android/Resources/Resource.designer.cs
+++ TurnamentManager/TurnamentManager.Android/Resources/Resource.designer.cs
@@ -10203,28 +10203,31 @@ namespace TurnamentManager.Droid
 			public const int notify_panel_notification_icon_bg = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int Team1 = 2131165306;
+			public const int playerIcon = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int teamm = 2131165307;
+			public const int Team1 = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int test = 2131165308;
+			public const int teamm = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int tooltip_frame_dark = 2131165309;
+			public const int test = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int tooltip_frame_light = 2131165310;
+			public const int tooltip_frame_dark = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int Tournament = 2131165311;
+			public const int tooltip_frame_light = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int Upic = 2131165312;
+			public const int Tournament = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int uploadImg = 2131165313;
+			public const int Upic = 2131165313;
+			
+			// aapt resource value: 0x7F070082
+			public const int uploadImg = 2131165314;
 			
 			static Drawable()
 			{
