f7df14c8eab1ca57f7a58f50fc7240b8c3b6f05a
jakkappro <jakkappro@gmail.com>
2021-03-14 15:55:50 +0100

    Prepare Tournament.cs to be saved

--- TurnamentManager/TurnamentManager/Classes/Tournament/Tournament.cs
+++ TurnamentManager/TurnamentManager/Classes/Tournament/Tournament.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections.Generic;
+using SQLite;
 
 namespace TurnamentManager.Classes.Tournament
 {
     public class Tournament
     {
-        public bool IsTeamBased;
-        public string Style;
-        public string Format;
+        public bool IsTeamBased { get; set; }
+        public string Style { get; set; }
+        public string Format { get; set; }
+        public string Name { get; set; }
+        
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
 
-        public readonly List<Team> Teams;
-
+        public List<int> PlayersID { get; set; }
+        
         public Tournament()
         {
-            Teams = new List<Team>();
+            PlayersID = new List<int>();
         }
-        
-        
     }
 }
--- TurnamentManager/TurnamentManager/Views/CreateTournamentPage.xaml.cs
+++ TurnamentManager/TurnamentManager/Views/CreateTournamentPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using TurnamentManager.Classes.Tournament;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TurnamentManager.Views
@@ -10,6 +12,27 @@ namespace TurnamentManager.Views
         {
             InitializeComponent();
         }
-       
+
+        private void SaveButton_OnClicked(object sender, EventArgs e)
+        {
+            var tournament = new Tournament()
+            {
+                Format = Format.SelectedIndex switch
+                {
+                    0 => "League",
+                    1 => "Groups",
+                    //....
+                    _ => throw new ArgumentOutOfRangeException()
+                },
+                IsTeamBased = false,//Add button to choose
+                Name = Name.Text,
+                Style = Sports.SelectedIndex switch
+                {
+                    0 => "Football",
+                    1 => "Basketball",
+                    _ => throw new ArgumentOutOfRangeException()
+                }
+            };
+        }
     }
 }
