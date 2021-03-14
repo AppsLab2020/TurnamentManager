c6215e58e7b4ce1e3bb0335cc3b9921ccfdedac1
FilipDavid1 <filipenkodavid@gmail.com>
2021-03-14 18:36:34 +0100

    Add frame with 2 buttons for match draw

--- TurnamentManager/TurnamentManager/Views/DrawPage.xaml.cs
+++ TurnamentManager/TurnamentManager/Views/DrawPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XLabs.Enums;
 
 namespace TurnamentManager.Views
 {
@@ -15,6 +16,47 @@ namespace TurnamentManager.Views
         public DrawPage()
         {
             InitializeComponent();
+            var st = new StackLayout { };
+            var frame = new Frame
+            {
+                CornerRadius = 20,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Start,
+                HasShadow = true,
+                IsClippedToBounds = true,
+                Padding = 0,
+                HeightRequest = 450,
+                WidthRequest = 300,
+                
+            };
+
+
+            var button1 = new Button
+            {
+                Text = "Random",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                CornerRadius = 5,
+                BorderWidth = 2,
+                BackgroundColor = Color.White
+            };
+            
+            var button2 = new Button
+            {
+                Text = "Manual",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                CornerRadius = 5,
+                BorderWidth = 2,
+                BackgroundColor = Color.White
+            };
+            
+            st.Children.Add(button1);
+            st.Children.Add(button2);
+            frame.Content = st;
+            Layout.Children.Add(frame);
         }
+
+        
     }
 }
