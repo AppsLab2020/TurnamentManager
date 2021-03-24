eabd1ebed2ea9120a843acd134812c7741b90bf2
jakkappro <jakkappro@gmail.com>
2021-03-24 18:20:51 +0100

    Refactor CreatePlayerPage to mvvm

--- TurnamentManager/TurnamentManager/Models/CreatePlayerModel.cs
+++ TurnamentManager/TurnamentManager/Models/CreatePlayerModel.cs
@@ -1,7 +1,109 @@
-﻿namespace TurnamentManager.Models
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using SQLite;
+using TurnamentManager.Classes.Tournament;
+using TurnamentManager.Views;
+using Xamarin.Forms;
+
+namespace TurnamentManager.Models
 {
-    public class CreatePlayerModel
+    public class CreatePlayerModel:INotifyPropertyChanged
     {
-        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand PickPhotoCommand { get; set; }
+
+        public ICommand SaveDataCommand { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int SelectedQuality { get; set; }
+
+        public ImageSource PickedImageSource
+        {
+            get => _pickImageSource;
+            set
+            {
+                if (value == _pickImageSource) 
+                    return;
+
+                _pickImageSource = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ImageSource _pickImageSource;
+
+        private string _imagePath;
+
+        private readonly INavigation _navigation;
+
+        public CreatePlayerModel(INavigation navigation)
+        {
+            _pickImageSource = "upload_pic.png";
+            PickPhotoCommand = new Command(PickImage);
+            SaveDataCommand = new Command(SaveData);
+            _navigation = navigation;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async void PickImage()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await Application.Current.MainPage.DisplayAlert("Not supported", "not supported", "ok");
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if (selectedImageFile == null)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "could not get image", "ok");
+                return;
+            }
+
+            PickedImageSource = ImageSource.FromStream(() => selectedImageFile.GetStream());
+            _imagePath = selectedImageFile.Path;
+        }
+
+        private async void SaveData()
+        {
+            var name = PlayerName;
+            var pathToImage = _imagePath;
+            var quality = SelectedQuality;
+            var playerQuality = quality switch
+            {
+                0 => Player.PlayerQuality.Flexible,
+                1 => Player.PlayerQuality.Strategic,
+                2 => Player.PlayerQuality.SharesTheirExpertise,
+                3 => Player.PlayerQuality.RespectfulToOthers,
+                4 => Player.PlayerQuality.ContributeIdeas,
+                _ => Player.PlayerQuality.Flexible
+            };
+
+            var player = new Player(name, pathToImage, playerQuality);
+
+            using var conn = new SQLiteConnection(Path.Combine(App.FolderPath, "players.db3"));
+            conn.CreateTable<Player>();
+            conn.Insert(player);
+
+            await _navigation.PopAsync();
+        }
     }
 }
--- TurnamentManager/TurnamentManager/Views/CreatePlayerPage.xaml.cs
+++ TurnamentManager/TurnamentManager/Views/CreatePlayerPage.xaml.cs
@@ -1,79 +1,35 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 using TurnamentManager.Classes.Tournament;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using SQLite;
+using TurnamentManager.Models;
 
 namespace TurnamentManager.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CreatePlayerPage : ContentPage
     {
-        private string _imagePath;
         public CreatePlayerPage()
         {
-            _imagePath = "";
+
             InitializeComponent();
+            BindingContext = new CreatePlayerModel(Navigation);
         }
-        
-        private async void MenuItem_OnClicked(object sender, EventArgs e)
-        {
-            await CrossMedia.Current.Initialize();
 
-            if (!CrossMedia.Current.IsPickPhotoSupported)
-            {
-                await DisplayAlert("Not supported", "not supported", "ok");
-            }
-
-            var mediaOptions = new PickMediaOptions()
-            {
-                PhotoSize = PhotoSize.Medium
-            };
-            
-            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
-            _imagePath = selectedImageFile.Path;
-            
-            if (selectedImage == null)
-            {
-                await DisplayAlert("Error", "could not get image", "ok");
-            }
-            
-            selectedImage.Source = ImageSource.FromStream(()=> selectedImageFile.GetStream());
-        }
-        
         private async void SaveButton_OnClicked(object sender, EventArgs e)
         {
-            await SaveButton.ScaleTo(1.2, 500, Easing.SpringOut); 
-            await SaveButton.ScaleTo(1, 300);
-            if (string.IsNullOrEmpty(_imagePath))
-            {
-                return; //Vypis ze chce obrazek !!!
-            }
             
-            var name = Name.Text;
-            var pathToImage = _imagePath;
-            var quality = Quality.SelectedIndex;
-            var playerQuality = quality switch
-            {
-                0 => Player.PlayerQuality.Flexible,
-                1 => Player.PlayerQuality.Strategic,
-                2 => Player.PlayerQuality.SharesTheirExpertise,
-                3 => Player.PlayerQuality.RespectfulToOthers,
-                4 => Player.PlayerQuality.ContributeIdeas,
-                _ => Player.PlayerQuality.Flexible
-                
-            };
+        }
 
-            var player = new Player(name, pathToImage, playerQuality);
-
-            using var conn = new SQLiteConnection(Path.Combine(App.FolderPath, "players.db3"));
-            conn.CreateTable<Player>();
-            conn.Insert(player);
-
-          await Navigation.PopAsync();
+        public async Task AnimateSaveButton()
+        {
+            await SaveButton.ScaleTo(1.2, 500, Easing.SpringOut);
+            await SaveButton.ScaleTo(1, 300);
         }
     }
 }
