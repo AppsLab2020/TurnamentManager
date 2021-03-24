23ab737a25d540da54ff9796eadafc3c5f228cb3
jakkappro <jakkappro@gmail.com>
2021-03-24 19:04:06 +0100

    Refactor CreateTournamentPage to mvvm

--- TurnamentManager/TurnamentManager/Models/CreateTournamentModel.cs
+++ TurnamentManager/TurnamentManager/Models/CreateTournamentModel.cs
@@ -1,7 +1,61 @@
-﻿namespace TurnamentManager.Models
+﻿using System;
+using System.IO;
+using System.Windows.Input;
+using SQLite;
+using TurnamentManager.Classes.Tournament;
+using Xamarin.Forms;
+
+namespace TurnamentManager.Models
 {
     public class CreateTournamentModel
     {
-        
+        public ICommand SaveDataCommand { get; set; }
+
+        public int Format { get; set; }
+        public int IsTeamBased { get; set; }
+        public int Sport { get; set; }
+
+        public string Name { get; set; }
+
+        private INavigation _navigation;
+
+        public CreateTournamentModel(INavigation navigation)
+        {
+            _navigation = navigation;
+            SaveDataCommand = new Command(SaveData);
+        }
+
+        private async void SaveData()
+        {
+            var tournament = new Tournament()
+            {
+                Format = Format switch
+                {
+                    0 => "Knockout",
+                    1 => "Groups",
+                    //....
+                    _ => throw new ArgumentOutOfRangeException()
+                },
+                IsTeamBased = IsTeamBased switch
+                {
+                    0 => true,
+                    1 => false,
+                    _ => throw new ArgumentOutOfRangeException()
+                },//Add button to choose
+                Name = Name,
+                Style = Sport switch
+                {
+                    0 => "Football",
+                    1 => "Basketball",
+                    _ => throw new ArgumentOutOfRangeException()
+                }
+            };
+
+            using var conn = new SQLiteConnection(Path.Combine(App.FolderPath, "tournaments.db3"));
+            conn.CreateTable<Tournament>();
+            conn.Insert(tournament);
+
+            await _navigation.PopAsync();
+        }
     }
 }
--- TurnamentManager/TurnamentManager/Views/CreateTournamentPage.xaml.cs
+++ TurnamentManager/TurnamentManager/Views/CreateTournamentPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using SQLite;
 using TurnamentManager.Classes.Tournament;
+using TurnamentManager.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,41 +14,7 @@ namespace TurnamentManager.Views
         public CreateTournamentPage()
         {
             InitializeComponent();
-        }
-
-       private async void SaveButton_OnClicked(object sender, EventArgs e)
-        { 
-            await SaveButton.ScaleTo(1.2, 500, Easing.SpringOut); 
-            await SaveButton.ScaleTo(1, 300);
-            var tournament = new Tournament()
-            {
-                Format = Format.SelectedIndex switch
-                {
-                    0 => "Knockout",
-                    1 => "Groups",
-                    //....
-                    _ => throw new ArgumentOutOfRangeException()
-                },
-                IsTeamBased = Type.SelectedIndex switch
-                {
-                    0 => true,
-                    1 => false,
-                    _ => throw new ArgumentOutOfRangeException()
-                },//Add button to choose
-                Name = Name.Text,
-                Style = Sports.SelectedIndex switch
-                {
-                    0 => "Football",
-                    1 => "Basketball",
-                    _ => throw new ArgumentOutOfRangeException()
-                }
-            };
-            
-            using var conn = new SQLiteConnection(Path.Combine(App.FolderPath, "tournaments.db3"));
-            conn.CreateTable<Tournament>();
-            conn.Insert(tournament);
-
-           await Navigation.PopAsync();
+            BindingContext = new CreateTournamentModel(Navigation);
         }
     }
 }
