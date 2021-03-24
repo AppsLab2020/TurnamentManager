f646fa57257de4f5d0b0932edd30ab7af455c97e
jakkappro <jakkappro@gmail.com>
2021-03-24 18:49:12 +0100

    Refactor CreateTeamPage to mvvm

--- TurnamentManager/TurnamentManager/Models/CreateTeamModel.cs
+++ TurnamentManager/TurnamentManager/Models/CreateTeamModel.cs
@@ -1,7 +1,63 @@
-﻿namespace TurnamentManager.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using SQLite;
+using TurnamentManager.Classes.Tournament;
+using Xamarin.Forms;
+
+namespace TurnamentManager.Models
 {
-    public class CreateTeamModel
+    public class CreateTeamModel : INotifyPropertyChanged
     {
-        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand AddPlayerCommand { get; set; }
+
+        public ObservableCollection<string> PlayerNamesCollection { get; set; }
+
+        private List<Player> _players;
+
+        public CreateTeamModel()
+        {
+            _players = new List<Player>();
+            PlayerNamesCollection = new ObservableCollection<string>();
+            AddPlayerCommand = new Command(AddPlayer);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async void AddPlayer()
+        {
+            using var conn = new SQLiteConnection(Path.Combine(App.FolderPath, "players.db3"));
+            conn.CreateTable<Player>();
+            var players = conn.Table<Player>().ToList();
+
+            var action = await Application.Current.MainPage.DisplayActionSheet("Choose player", "Cancel", null,
+                players.Select(player => player.Name).ToArray());
+
+            var exist = false;
+
+            foreach (var player in _players.Where(player => player.Name == action))
+                exist = true;
+
+            if (action == "cancel" || string.IsNullOrWhiteSpace(action) || exist)
+                return;
+
+            foreach (var player in players.Where(player => player.Name == action))
+            {
+                _players.Add(player);
+                PlayerNamesCollection.Add(player.Name);
+            }
+        }
     }
 }
+
+//TODO: Add removing players, saving
--- TurnamentManager/TurnamentManager/Views/CreateTeamPage.xaml.cs
+++ TurnamentManager/TurnamentManager/Views/CreateTeamPage.xaml.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using SQLite;
-using TurnamentManager.Classes.Tournament;
+using TurnamentManager.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,53 +8,15 @@ namespace TurnamentManager.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CreateTeamPage : ContentPage
     {
-        private static ICommand RemoveCommand => new Command<int>(RemovePlayer);
-
-        public static ObservableCollection<Player> Players;
         public CreateTeamPage()
         {
             InitializeComponent();
-            Players = new ObservableCollection<Player>();
-            PlayersDisplay.ItemsSource = Players;
+            BindingContext = new CreateTeamModel();
         }
 
-        private void AddPlayerButton_OnClicked(object sender, EventArgs e)
+        private async void SaveButton_OnClicked(object sender, EventArgs e)
         {
-            OnActionSheetSimpleClicked(sender, e);
-        }
-
-        private async void OnActionSheetSimpleClicked(object sender, EventArgs e)
-        {
-            using var conn = new SQLiteConnection(Path.Combine(App.FolderPath, "players.db3"));
-            conn.CreateTable<Player>();
-            var players = conn.Table<Player>().ToList();
-
-            var action = await DisplayActionSheet ("Choose player", "Cancel", null, players.Select(player => player.Name).ToArray());
-
-            var exist = false;
-
-            foreach (var player in Players.Where(player => player.Name == action))
-                exist = true;
-
-            if (action == "cancel" || string.IsNullOrWhiteSpace(action) || exist)
-                return;
-            
-            foreach (var player in players.Where(player => player.Name == action))
-                Players.Add(player);
-        }
-
-        private static void RemovePlayer(int id)
-        {
-            foreach (var player in Players.Where(player => player.ID == id))
-            {
-                Players.Remove(player);
-                break;
-            }
-        }
-
-      private async void SaveButton_OnClicked(object sender, EventArgs e)
-        {
-            await SaveButton.ScaleTo(1.2, 500, Easing.SpringOut); 
+            await SaveButton.ScaleTo(1.2, 500, Easing.SpringOut);
             await SaveButton.ScaleTo(1, 300);
         }
     }
