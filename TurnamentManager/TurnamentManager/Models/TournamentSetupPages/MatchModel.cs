ee4edf0f73c19d3f3a9df3e8e253184917fa02e1
jakkappro <jakkappro@gmail.com>
2021-05-21 17:02:34 +0200

    Add first phase rendering to MatchModel.cs

--- TurnamentManager/TurnamentManager/Models/TournamentSetupPages/MatchModel.cs
+++ TurnamentManager/TurnamentManager/Models/TournamentSetupPages/MatchModel.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows.Input;
+using SQLite;
+using TurnamentManager.Classes.Tournament;
 using Xamarin.Forms;
 
 namespace TurnamentManager.Models
@@ -9,13 +14,20 @@ namespace TurnamentManager.Models
 
         private ICommand TapCommand;
 
-        private int[] currentCoords;
-        
-        
-        
+        private const int _frameWidth = 75;
+        private const int _frameHeight = 20;
+
+        private const int _xShift = 80;
+        private const int _yShift = 80;
+
+        private readonly int _tournamentId;
+
+        private List<string> _matchesList;
+
         public MatchModel(INavigation navigation, int tournamentId)
         {
-            currentCoords = new[] {0, 0};
+            _matchesList = new List<string>();
+            _tournamentId = tournamentId;
         }
 
         public ScrollView GetSpider()
@@ -28,9 +40,26 @@ namespace TurnamentManager.Models
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 BackgroundColor = Color.Red
             };
-            
-            layout.Children.Add(GetFrame("test", "test"), new Point(15,15));
-            
+
+            using var conn = new SQLiteConnection(Path.Combine(App.FolderPath, "tournaments.db3"));
+            conn.CreateTable<Tournament>();
+            var tournaments = conn.Table<Tournament>().ToList();
+
+            foreach (var tournament in tournaments.Where(tournament => tournament.ID == _tournamentId))
+            {
+                _matchesList = tournament.MatchesString.Split('\n').ToList();
+            }
+
+            _matchesList.RemoveAt(_matchesList.Count - 1);
+
+            for (var i = 0; i < _matchesList.Count; i++ )
+            {
+                var match = _matchesList[i];
+                var names = match.Split(' ');
+                
+                layout.Children.Add(GetFrame(names[0], names[2]), new Point(15, 15 + (_yShift * i)));
+            }
+
             var scroll = new ScrollView
             {
                 Orientation = ScrollOrientation.Vertical,
@@ -54,14 +83,6 @@ namespace TurnamentManager.Models
                 VerticalOptions = LayoutOptions.CenterAndExpand,
                 Text = leftName,
             };
-            var vsImage = new Image
-            {
-                HeightRequest = 65,
-                BackgroundColor = Color.Transparent,
-                HorizontalOptions = LayoutOptions.Start,
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                Source = "vs_image.png"
-            };
             var addButton2 = new Label
             {
                 HeightRequest = 50,
@@ -82,14 +103,13 @@ namespace TurnamentManager.Models
             var st = new StackLayout
             {
                 Orientation = StackOrientation.Horizontal,
-                WidthRequest = 300,
-                HeightRequest = 80,
+                WidthRequest = _frameWidth,
+                HeightRequest = _frameHeight,
             };
             
             var tap = new TapGestureRecognizer { Command = TapCommand};
 
             st.Children.Add(addButton1);
-            st.Children.Add(vsImage);
             st.Children.Add(addButton2);
             frame.Content = st;
 
