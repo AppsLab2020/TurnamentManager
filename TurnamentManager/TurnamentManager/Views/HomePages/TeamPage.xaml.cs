b15ce4b6ab3098f5e56e6c4f3c1ba1d3c7ad1e0e
FilipDavid1 <filipenkodavid@gmail.com>
2021-05-29 16:05:23 +0200

    Fix color

--- TurnamentManager/TurnamentManager/Views/HomePages/TeamPage.xaml.cs
+++ TurnamentManager/TurnamentManager/Views/HomePages/TeamPage.xaml.cs
@@ -13,12 +13,11 @@ namespace TurnamentManager.Views
             BindingContext = new TeamModel(Navigation);
         }
 
-        private async void Button_OnClicked(object sender, EventArgs e)
+        /*private async void Button_OnClicked(object sender, EventArgs e)
         {
             await PlusButton.TranslateTo(10, 0, 500, Easing.BounceOut);
             await PlusButton.TranslateTo(0, 0);
-            await Navigation.PushAsync(new CreateTeamPage());
-        }
+        }*/
         
     }
 }
