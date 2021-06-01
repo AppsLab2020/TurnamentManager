cbdd55a41ff99b1e4c8791402d0ed0b5487f55f2
jakkappro <jakkappro@gmail.com>
2021-06-01 10:47:02 +0200

    add RandomPage.xaml structure

---
+++ TurnamentManager/TurnamentManager/Views/TournamentSetupPages/RandomPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TurnamentManager.Views.TournamentSetupPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RandomPage : ContentPage
+    {
+        public RandomPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
